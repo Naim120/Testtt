d25667ffe04241f31204ffeabcc2adc0ba191274
sk naim <sknaimuddin234@gmail.com>
2023-07-12 23:47:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function openCarousel() {
-  var carousel = document.querySelector(".carousel");
-  carousel.style.display = "block";
+  var carousel = document.createElement("div");
+  carousel.classList.add("carousel");
+
+  var imgs = [
+    "image.jpg",
+    "image.jpg",
+    "image.jpg",
+    "image.jpg",
+    "image.jpg",
+  ];
+
+  for (var i = 0; i < imgs.length; i++) {
+    var img = document.createElement("img");
+    img.src = imgs[i];
+    carousel.appendChild(img);
+  }
+
+  document.body.appendChild(carousel);
 }
 
 document.querySelector(".card").addEventListener("click", openCarousel);
